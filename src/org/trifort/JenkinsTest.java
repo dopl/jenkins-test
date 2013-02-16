e7daf46bb9f5d64933d6c15cfed98b5f17f0c1e1
Phil Pratt-Szeliga <pcpratts@chirrup.org>
2013-02-16 12:02:28 -0500

    testing jenkins again

--- src/org/trifort/JenkinsTest.java
+++ src/org/trifort/JenkinsTest.java
@@ -3,6 +3,6 @@ package org.trifort;
 class JenkinsTest {
   public static void main(String[] args){
     System.out.println("hello world!");
-    System.out.println("jenkins test");
+    System.out.println("jenkins test!");
   }
 }
